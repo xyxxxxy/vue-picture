0901733d3704c86b38f4f7e729b0e70e7687dd13
xyxxxxy <2403876746@qq.com>
2025-07-07 11:28:03 +0800

    Initial commit

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="globalHeader">
+    <a-row :wrap="false">
+      <a-col flex="200px">
+        <RouterLink to="/">
+          <div class="title-bar">
+            <img class="logo" src="../assets/logo.svg" alt="logo" />
+            <div class="title">鱼皮云图库</div>
+          </div>
+        </RouterLink>
+      </a-col>
+      <a-col flex="auto">
+        <a-menu v-model:selectedKeys="current" mode="horizontal" :items="items" />
+      </a-col>
+      <a-col flex="120px">
+        <div class="user-login-status">
+          <a-button type="primary" href="/user/login">登录</a-button>
+        </div>
+      </a-col>
+    </a-row>
+  </div>
+</template>
+<script lang="ts" setup>
+import { h, ref } from 'vue'
+import { HomeOutlined } from '@ant-design/icons-vue'
+import type { MenuProps } from 'ant-design-vue'
+const current = ref<string[]>(['home'])
+const items = ref<MenuProps['items']>([
+  {
+    key: '/',
+    icon: () => h(HomeOutlined),
+    label: '主页',
+    title: '主页',
+  },
+  {
+    key: '/about',
+    label: '关于',
+    title: '关于',
+  },
+  {
+    key: 'others',
+    label: h('a', { href: 'http://www.codefather.cn', target: '_blank' }, '编程导航'),
+    title: '编程导航',
+  },
+])
+</script>
+<style scoped>
+.title-bar {
+  display: flex;
+  align-items: center;
+}
+
+.title {
+  color: black;
+  font-size: 18px;
+  margin-left: 16px;
+}
+
+.logo {
+  height: 48px;
+}
+</style>
---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="min-height: 100vh">
+      <a-layout-header class="header"><GlobalHeader /></a-layout-header>
+      <a-layout-content class="content"><router-view /></a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="http://www.codefather.cn" target="_blank"> 编程导航1 </a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<script setup lang="ts">
+import GlobalHeader from '@/components/GlobalHeader.vue'
+</script>
+<style scoped>
+#basicLayout.footer {
+  background: #fd0505;
+  padding: 16px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+#basicLayout.content {
+  background: linear-gradient(to right, #fefefe, #fff);
+  margin-bottom: 28px;
+  padding: 20px;
+}
+#basicLayout .header {
+  padding-inline: 20px;
+  margin-bottom: 16px;
+  color: unset;
+  background: white;
+}
+</style>
